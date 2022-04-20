566468cf9bdfe63fe5d2ad1365540b8ee3e0f5e0
SSto <95592688+sstover25@users.noreply.github.com>
2022-04-20 12:25:19 -0500

    created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+const { Schema, model, Types } = require("mongoose");
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // getter method to modify date here
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
