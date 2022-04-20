26c183460e91b1e23d226da3ec2838715a3d6a53
SSto <95592688+sstover25@users.noreply.github.com>
2022-04-20 14:36:10 -0500

    added GET routes for thought model

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,7 +2,25 @@ const { Thought, User } = require("../models");
 
 const thoughtController = {
   // GET all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
   // GET a single thought  by its _id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
   // POST/Create a new thought
   // PUT/Update a thought by its _id
   // DELETE a thought by its _id
